75ffe2ef1f0127bbca8a224e82f4015e6efd2613
umermemon4648 <umermemon4648@gmail.com>
2023-06-03 22:30:12 +0500

    refactoring the written logic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const setBudget = document.querySelector('#set-budget')
 const setExpense = document.querySelector('#set-expense-amount')
 const setBalance = document.querySelector('#remaining-balance')
+let tableBody = document.querySelector('#table-body')
 
 
 //  ****** Input fields *****
@@ -20,42 +21,18 @@ const budgetDetailForm = document.querySelector('#budget-detail-form')
 
 //  ****** button *****
 const setBudgetBtn = document.querySelector('#budget-btn')
+let expenseBtn = document.querySelector('#expense-btn')
+expenseBtn.textContent = "Add Expense"
 
 
 
 //  ****** some Useful variables *****
 let expenseList = []
 let totalExpense = 0
-let isEdit = false
+let isSelectedRow = null
 let isSetBudget = false
 
 
-// setBudgetForm.addEventListener('submit', (e) => {
-//   e.preventDefault()
-//     var value = parseFloat(budgetLimit.value);
-//     if (isNaN(value) || value < 0) {
-//         budgetLimit.value = '';
-//       return alert("-ve value is not allowed")
-
-//     }
-//     else if(setBudget.textContent != '0.00'){
-//       // console.log(setBudget.innerHTML)
-//       setBudgetBtn.disabled = true;
-//       setBudgetBtn.style.cursor = 'not-allowed';
-//       setBudgetBtn.style.opacity = 0.2;
-//           budgetLimit.readOnly = true;
-//           budgetLimit.style.opacity = 0.2;
-//           budgetLimit.style.border = 'none';
-//           return
-
-//     }
-
-//     else {
-//       setBudget.innerHTML = budgetLimit.value;
-//       setBudgetForm.reset()
-//     } 
-    
-//   });
 
 
 
@@ -82,30 +59,47 @@ setBudgetForm.addEventListener('submit', (e) => {
 // when expenseBtn hit, get all input values
 budgetDetailForm.addEventListener('submit', (e)=>{
   e.preventDefault()
-  let date = currentDate.value
-  let amount = Number(amountField.value)
-  let category = categoryField.value
-  let desc = descField.value
+  const formFields = {
+     date : currentDate.value,
+     amount : Number(amountField.value),
+     category : categoryField.value,
+     desc : descField.value,
+     id : Date.now(),
+  }
   // creating random id for each item
-  let id = Date.now()
 
 
-  if (date ==='' || category==='' || desc==='' ) {
+  if (formFields.date ==='' || formFields.category==='' || formFields.desc==='' ) {
     alert('Please complete all input fields')
     return
   }
-  else if((isNaN(amount) || amount<0)){
+  else if((isNaN(formFields.amount) || formFields.amount<0)){
     alert('Please complete amount fields')
     return
   }
+else if(isEdit){
+  let updatedFields = {
+    u_date:  currentDate.value,
+    u_amount:  Number(amountField.value),
+    u_category:  categoryField.value,
+    u_desc:  descField.value,
+    u_id:  Date.now(),
 
-else if(!isEdit){
-  expenseList.unshift({id, date, amount, category, desc})
+  }
+
+  console.log("updatedFields: ", updatedFields)
+  // addingRowsToTable(u_date, u_category, u_desc, u_amount, u_id)
+// editRow()
+}
+
+
+else{
+  expenseList.unshift(formFields)
   
-  totalExpense += amount
+  totalExpense += formFields.amount
   setExpense.textContent = totalExpense
   console.log(expenseList);
-  addingRowsToTable(date, category, desc, amount, id)
+  addingRowsToTable(formFields)
 }
 
 
@@ -118,8 +112,9 @@ isEdit = false;
 
 
 // **********  Function to insert new rows to the table ***********
-function addingRowsToTable(date, category, desc, amount, id){
-  let tableBody = document.querySelector('#table-body')
+function addingRowsToTable(formFields){
+  console.log("addingRowsToTable: ", formFields);
+  let {date, category, desc, amount, id} = formFields
   let row = tableBody.insertRow(0)
   
   // Add the data-id attribute with the id value
@@ -141,59 +136,15 @@ function addingRowsToTable(date, category, desc, amount, id){
 }
 
 
-// **********  Function to delete a row ***********
-const deletingRow = (id) => {
-  // Find the row to delete based on the id
+// **********  Deleting a row ***********
 
-  let tableBody = document.querySelector('#table-body');
 
-  // Find all rows within the table body
-  let rows = tableBody.querySelectorAll('tr');
 
-  // console.log("rows:", rows)
-  let rowsToDelete = Array.from(rows).find((expectedRow)=> expectedRow.getAttribute('data-id')===String(id))
 
-  // Delete the row from the table
-  if (rowsToDelete) {
-    tableBody.removeChild(rowsToDelete);
+// **********  Function to create a row ***********
 
-    
-  // update expenseList array whenever a row is deleted 
-  expenseList = expenseList.filter((exp)=> exp.id !== id)
-  // console.log("expenseList_after delteing ",expenseList);
-
-  // update Totalexpense section after deleting row
-  totalExpense = expenseList.reduce((acc, exp)=> acc+exp.amount, 0)
-  setExpense.textContent = totalExpense
-
-  }
-
+const createTableRow = ()=>{
+  const
 }
 
-
-
-// **********  Function to delete a row ***********
-const editRow = (id) => {
-  isEdit = true
-  // Find the row to edit based on the id
-
-  let tableBody = document.querySelector('#table-body');
-  let rowToEdit = tableBody.querySelector(`tr[data-id="${id}"]`);
-
-  // Find all rows within the table body
-  // let rows = tableBody.querySelectorAll('tr');
-
-  // console.log("rows:", rows)
-  if (rowToEdit) {
-    let tdElements = rowToEdit.querySelectorAll('td');
-    let tdValues = Array.from(tdElements).map(td => td.innerHTML);
-    console.log("tdValues:", tdValues);
-    currentDate.value = tdValues[0]
-    categoryField.value = tdValues[1]
-    descField.value = tdValues[2]
-    amountField.value = tdValues[3]
-  }
-
-
-}
 
